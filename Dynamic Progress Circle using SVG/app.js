dafc5619f711976e0bc2112367f449fb14eebf7b
BHASKAR PUROHIT <63875407+BhaskarPurohit@users.noreply.github.com>
2023-01-09 17:56:03 +0530

    Update app.js
    final commit

--- Dynamic Progress Circle using SVG/app.js
+++ Dynamic Progress Circle using SVG/app.js
@@ -1,1 +1,22 @@
-javascript
+const progress = document.querySelector(".progress");
+const loading = document.querySelector(".loading");
+
+let i = 0;
+const uploadPercent = [0, 10, 25, 40, 42, 60, 70, 75, 90, 100];
+
+
+
+const circle = document.querySelector(".progress-circle");
+
+const circumference = circle.getTotalLength();
+
+const interval = setInterval(() => {
+  circle.style.strokeDashoffset =
+    circumference - (uploadPercent[i] / 100) * circumference;
+  loading.innerHTML = uploadPercent[i] + "%";
+  i++;
+  if (i == uploadPercent.length) {
+    clearInterval(interval);
+    loading.innerHTML = "DONE!";
+  }
+}, 1000);
