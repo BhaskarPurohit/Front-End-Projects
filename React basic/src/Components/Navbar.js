8b10efddaae5f9402733f267dd6ad17ee0662754
BHASKAR PUROHIT <63875407+BhaskarPurohit@users.noreply.github.com>
2023-02-06 14:47:19 +0530

    Create Navbar.js
    6th feb

---
+++ React basic/src/Components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img src="./img/react.png" alt="" />
+      <h3 className="nav--logo_text">React Facts</h3>
+      <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
+  );
+};
+
+export default Navbar;
