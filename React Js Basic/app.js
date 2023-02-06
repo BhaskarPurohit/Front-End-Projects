23a63e6a995aa39f064a35fe3c048f5c3506eb4e
BHASKAR PUROHIT <63875407+BhaskarPurohit@users.noreply.github.com>
2023-02-06 11:37:57 +0530

    Delete app.js

--- React Js Basic/app.js
+++
@@ -1,7 +0,0 @@
-const boxContainer = document.querySelector("#boxContainer");
-
-for (let i = 0; i < 365; i++) {
-  const el = document.createElement("div");
-  el.classList = "box";
-  boxContainer.appendChild(el);
-}
