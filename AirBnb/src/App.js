f3bb0294f77d3842c0f1e981fdbb7c5ef68ec1da
BHASKAR PUROHIT <63875407+BhaskarPurohit@users.noreply.github.com>
2023-02-08 23:11:47 +0530

    Create App.js
    8th feb

---
+++ AirBnb/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+// import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
