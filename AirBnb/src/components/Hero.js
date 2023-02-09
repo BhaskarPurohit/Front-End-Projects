f7947eb945c48e53c623e0e4f0bbbbbb515f50d2
BHASKAR PUROHIT <63875407+BhaskarPurohit@users.noreply.github.com>
2023-02-09 23:22:57 +0530

    Update Hero.js
    9th feb

--- AirBnb/src/components/Hero.js
+++ AirBnb/src/components/Hero.js
@@ -3,11 +3,18 @@ import "./Hero.css";
 
 const Hero = () => {
   return (
-    <img
-      className="main--banner"
-      src={require("../img/main-banner.png")}
-      alt=""
-    />
+    <section>
+      <img
+        className="main--banner"
+        src={require("../img/main-banner.png")}
+        alt=""
+      />
+      <h1 className="hero--header">Online Experiences</h1>
+      <p className="hero--text">
+        Join uniques interactive activities led by one-of-a-kind hosts-all
+        without leaving home.
+      </p>
+    </section>
   );
 };
 
