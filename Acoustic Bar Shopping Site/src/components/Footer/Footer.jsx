d71695e5efdaf231c8c6beedf76d42e010df6912
BHASKAR PUROHIT <63875407+BhaskarPurohit@users.noreply.github.com>
2023-02-20 23:00:22 +0530

    Update Footer.jsx

--- Acoustic Bar Shopping Site/src/components/Footer/Footer.jsx
+++ Acoustic Bar Shopping Site/src/components/Footer/Footer.jsx
@@ -1,6 +1,47 @@
 import "./Footer.scss";
+import React from "react";
+import { FaLocationArrow, FaMobileAlt, FaEnvelope } from "react-icons/fa";
+import Payment from "../../assets/payments.png";
 const Footer = () => {
-    return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="col">
+          <div className="title">About</div>
+          <div className="text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum
+            quasi ipsam voluptatum praesentium consectetur aliquam doloribus
+            voluptatibus quisquam cupiditate vel?
+          </div>
+        </div>
+        <div className="col">
+          <div className="title">Contact</div>
+          <div className="c-item">
+            <FaLocationArrow />
+            <div className="text">
+              Kayaloram Rd, Punnamada, KOttankulangara, Alapuzza Kerala, 68888
+            </div>
+          </div>
+          <div className="c-item">
+            <FaMobileAlt />
+            <div className="text">Phone: 1800 900 0101</div>
+          </div>
+          <div className="c-item">
+            <FaEnvelope />
+            <div className="text">
+              Email: business.aucoustic-bar@hotmail.com
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="title">Categories</div>
+        </div>
+        <div className="col">
+          <div className="title">Pages</div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
