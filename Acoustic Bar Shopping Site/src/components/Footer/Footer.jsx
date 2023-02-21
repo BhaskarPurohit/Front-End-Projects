28dcd81fb7516d3b4bf20f7e943a8dc4551c79a8
BHASKAR PUROHIT <63875407+BhaskarPurohit@users.noreply.github.com>
2023-02-21 23:52:39 +0530

    Update Footer.jsx

--- Acoustic Bar Shopping Site/src/components/Footer/Footer.jsx
+++ Acoustic Bar Shopping Site/src/components/Footer/Footer.jsx
@@ -9,9 +9,9 @@ const Footer = () => {
         <div className="col">
           <div className="title">About</div>
           <div className="text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum
-            quasi ipsam voluptatum praesentium consectetur aliquam doloribus
-            voluptatibus quisquam cupiditate vel?
+            This is a clone website for shopping products such as headphones,
+            smartwatches, noise cancelling devices. I took inspiration from
+            website Boat, Sennhizer and Sony.
           </div>
         </div>
         <div className="col">
@@ -19,25 +19,43 @@ const Footer = () => {
           <div className="c-item">
             <FaLocationArrow />
             <div className="text">
-              Kayaloram Rd, Punnamada, KOttankulangara, Alapuzza Kerala, 68888
+              Hinjewadi Phase 2, IT Park, Pune Maharashtra
             </div>
           </div>
           <div className="c-item">
             <FaMobileAlt />
-            <div className="text">Phone: 1800 900 0101</div>
+            <div className="text">Phone: 9509627038</div>
           </div>
           <div className="c-item">
             <FaEnvelope />
-            <div className="text">
-              Email: business.aucoustic-bar@hotmail.com
-            </div>
+            <div className="text">Email: bhaskarpurohit22@gmail.com</div>
           </div>
         </div>
         <div className="col">
           <div className="title">Categories</div>
+          <div className="text">Headphones</div>
+          <div className="text">Smart Watches</div>
+          <div className="text">Bluetooth Speakers</div>
+          <div className="text">Wireless Earbuds</div>
+          <div className="text">Home THeatre</div>
+          <div className="text">Projectors</div>
         </div>
         <div className="col">
           <div className="title">Pages</div>
+          <span className="text">Home</span>
+          <span className="text">About</span>
+          <span className="text">Privacy Policy</span>
+          <span className="text">Returns</span>
+          <span className="text">Terms & Conditions</span>
+          <span className="text">Contact Us</span>
+        </div>
+      </div>
+      <div className="bottom-bar">
+        <div className="bottom-bar-content">
+          <div className="text">
+            Acoutics Bar 2023 created by BHASKAR PUROHIT, Front End Developer
+          </div>
+          <img src={Payment} alt="" />
         </div>
       </div>
     </footer>
