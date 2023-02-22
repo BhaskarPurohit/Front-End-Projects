dcbabd7ad5ea94bc56baf066a8016f024c2d627e
BHASKAR PUROHIT <63875407+BhaskarPurohit@users.noreply.github.com>
2023-02-22 23:58:56 +0530

    Update Banner.jsx

--- Acoustic Bar Shopping Site/src/components/Home/Banner/Banner.jsx
+++ Acoustic Bar Shopping Site/src/components/Home/Banner/Banner.jsx
@@ -1,26 +1,28 @@
+import React from "react";
+
 import "./Banner.scss";
 import BannerImg from "../../../assets/banner-img.png";
+
 const Banner = () => {
-  return (
-    <div className="hero-banner">
-      <div className="content">
-        <div className="text-content">
-          <h1>SALES</h1>
-          <p>
-            {" "}
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Consequatur, maxime aperiam. Vero sit provident autem error
-            laudantium dolorem? Et, harum.{" "}
-          </p>
-          <div className="ctas">
-            <div className="banner-cta">Read More</div>
-            <div className="banner-cta v2">Shop Now</div>
-          </div>
-          <img className="banner-img" src={BannerImg} alt="" />
+    return (
+        <div className="hero-banner">
+            <div className="content">
+                <div className="text-content">
+                    <h1>SALES</h1>
+                    <p>
+                        Convallis interdum purus adipiscing dis parturient
+                        posuere ac a quam a eleifend montes parturient posuere
+                        curae tempor
+                    </p>
+                    <div className="ctas">
+                        <div className="banner-cta">Read More</div>
+                        <div className="banner-cta v2">Shop Now</div>
+                    </div>
+                </div>
+                <img className="banner-img" src={BannerImg} />
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Banner;
