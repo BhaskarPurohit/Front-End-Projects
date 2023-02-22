df7e1411e2f70402b23a5fa2e110596bb9241eaf
BHASKAR PUROHIT <63875407+BhaskarPurohit@users.noreply.github.com>
2023-02-23 00:26:15 +0530

    Update Products.jsx

--- Acoustic Bar Shopping Site/src/components/Products/Products.jsx
+++ Acoustic Bar Shopping Site/src/components/Products/Products.jsx
@@ -1,6 +1,44 @@
 import "./Products.scss";
+import Product from "./Product/Product";
 const Products = () => {
-    return <div>Products</div>;
+  return (
+    <div className="products-container">
+      <div className="sec-heading">Section Heading</div>
+      <div className="products">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
+    </div>
+  );
+};
+
+export default Products;
+import "./Products.scss";
+import Product from "./Product/Product";
+const Products = () => {
+  return (
+    <div className="products-container">
+      <div className="sec-heading">Section Heading</div>
+      <div className="products">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
+    </div>
+  );
 };
 
 export default Products;
